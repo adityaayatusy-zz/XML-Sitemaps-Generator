878335d9c55b7ce5c58e692149eb363be4254171
aditya <adityaaytusy@gmail.com>
2019-05-22 10:30:07 +0700

    add autoload

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
-    require_once 'core/Sitemap.php';
-    
+    //autoload
+    require_once 'autoload.php';
+    //timeout max 5 minutes
+    set_time_limit (18000);
+
     $smp = new Sitemap('https://google.com',__DIR__);
     $smp->img_url = true;
-    $smp->external_url = false;
+    $smp->external_url = true;
     $smp->create();
